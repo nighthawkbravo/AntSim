c85e731289611e89186da6cceea96db22f124658
VIZaneIV <72400206+VIZaneIV@users.noreply.github.com>
2021-05-19 13:54:25 +0200

    Ant start at the colony, only 1 colony

---
+++ WpfAntSimulator/HelpersAndEventHandlers.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using WpfAntSimulator.SimObjects;
+using Bitmap = System.Drawing.Bitmap;
+using Point = System.Drawing.Point;
+
+namespace WpfAntSimulator
+{
+    public partial class MainWindow
+    {
+        // ################## Helper functions ##################
+        private void ResetSimButton(object sender, RoutedEventArgs e)
+        {
+            simObjects.Clear();
+            simStaticObjects.Clear();
+            simInit = false;
+            RenderStatics();
+            RenderAll();
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private Bitmap MergedBitmaps(Bitmap bmp1, Bitmap bmp2)
+        {
+            Bitmap result = new Bitmap(Math.Max(bmp1.Width, bmp2.Width),
+                                       Math.Max(bmp1.Height, bmp2.Height));
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp2, Point.Empty);
+                g.DrawImage(bmp1, Point.Empty);
+            }
+            return result;
+        }
+
+        // ################## Event handlers ##################
+
+        private void SelectObstacle_Click(object sender, RoutedEventArgs e)
+        {
+            selectedObject = new Obstacle();
+        }
+        private void SelectNest_Click(object sender, RoutedEventArgs e)
+        {
+            selectedObject = new Colony();
+        }
+
+        private void myImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selectedObject == null) return;
+
+            bool changeWasMade = false;
+            var prevSelectedObject = selectedObject;
+            Point prevPos = selectedObject.Position;
+
+            selectedObject.Position = new Point((int)e.GetPosition(myImage).X, (int)e.GetPosition(myImage).Y);
+            switch (selectedObject)
+            {
+                case Obstacle obstacle:
+                    int prevW = (selectedObject as Obstacle).Width;
+                    int prevH = (selectedObject as Obstacle).Height;
+
+                    int newW = Int32.Parse(ObstacleWidth.Text);
+                    int newH = Int32.Parse(ObstacleHeight.Text);
+
+                    (selectedObject as Obstacle).Width = newW;
+                    (selectedObject as Obstacle).Height = newH;
+
+                    if (prevW != newW || prevH != newH || AreDifferent(prevPos, selectedObject.Position))
+                    {
+                        changeWasMade = true;
+                    }
+
+                    break;
+                case Colony colony:
+                    int prevR = (selectedObject as Colony).Radius;
+                    int newR = Int32.Parse(NestRadius.Text);
+                    (selectedObject as Colony).Radius = newR;
+
+                    simStaticObjects.Remove(OriginalColony);
+                    simStaticObjects.Add(OriginalColony = new Colony(selectedObject.Position, newR));
+
+                    if (prevR != newR || AreDifferent(prevPos, selectedObject.Position))
+                    {
+                        changeWasMade = true;
+                    }
+
+                    break;
+                default:
+                    return;
+            }
+            if (!simStaticObjects.Contains(selectedObject) || changeWasMade)
+            {
+                if (changeWasMade)
+                {
+                    simStaticObjects.Remove(prevSelectedObject);
+                    changeWasMade = false;
+                }
+                simStaticObjects.Add(selectedObject);
+                RenderStatics();
+            }
+            RenderAll();
+        }
+
+
+        private bool AreDifferent(Point a, Point b)
+        {
+            if (a.X != b.X) return true;
+            if (a.Y != b.Y) return true;
+            return false;
+        }
+
+
+    }
+}
--- WpfAntSimulator/MainWindow.xaml.cs
+++ WpfAntSimulator/MainWindow.xaml.cs
@@ -1,29 +1,18 @@
 ﻿
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 using System;
-using System.Diagnostics;
-using System.Windows;
-using System.Windows.Threading;
-using Bitmap = System.Drawing.Bitmap;
-using Color = System.Drawing.Color;
-using Point = System.Drawing.Point;
-using System.IO;
-using System.Drawing.Imaging;
-using WpfAntSimulator.SimObjects;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using WpfAntSimulator.SimObjects;
+using Bitmap = System.Drawing.Bitmap;
+using Point = System.Drawing.Point;
 
 namespace WpfAntSimulator
 {
@@ -52,8 +41,8 @@ namespace WpfAntSimulator
         public static Direction NumToDir(int i)
         {
             return directions[i];
-        }        
-    }      
+        }
+    }
 
     public partial class MainWindow : Window
     {
@@ -75,6 +64,7 @@ namespace WpfAntSimulator
 
         private Random rnd;
 
+        private Colony OriginalColony;
         private List<ISimObject> toBeRemoved = new List<ISimObject>();
         private List<ISimObject> simObjects;
         private List<ISimObject> simStaticObjects = new List<ISimObject>();
@@ -94,57 +84,19 @@ namespace WpfAntSimulator
             numOfAnts = Int32.Parse(AntAmount.Text);
             bmStatic = new Bitmap(width, height);
             RenderAll();
+            simStaticObjects.Add(OriginalColony = new Colony(center, 3));
+            RenderStatics();
+            RenderAll();
         }
 
-        private void UpdateAll()
-        {
-            foreach (var simObj in simObjects)
-            {
-                if (!simObj.ShouldBeRendered())
-                {
-                    toBeRemoved.Add(simObj);
-                    continue;
-                }
-                simObj.Update();
-            }
-            if (toBeRemoved.Count > 0)
-            {
-                foreach (var o in toBeRemoved)
-                {
-                    simObjects.Remove(o);
-                }
-                toBeRemoved.Clear();
-            }
-        }
-        private void RenderStatics()
-        {
-            bmStatic = new Bitmap(width, height);
 
-            foreach (var simObj in simStaticObjects)
-            {
-                simObj.Render(bmStatic);
-            }
-        }
-
-        private void RenderAll()
-        {
-            bm = new Bitmap(width, height);            
-            //bm = bmStatic;
-            foreach (var simObj in simObjects)
-            {
-                simObj.Render(bm);
-            }
-            var res = MergedBitmaps(bmStatic, bm);
-            DisplayImage(res);
-        }
         private void InitSim()
         {
-            simObjects.Add(new Colony(center));
-            for (int i=0; i<numOfAnts; ++i)
+            for (int i = 0; i < numOfAnts; ++i)
             {
                 // Random rand = new Random(Guid.NewGuid().GetHashCode()); // Very useful for generating random objects with random seeds!
-                simObjects.Add(new Ant(Globals.NumToDir(rnd.Next(Globals.directions.Count)), center, new Random(Guid.NewGuid().GetHashCode())));
-                
+                simObjects.Add(new Ant(Globals.NumToDir(rnd.Next(Globals.directions.Count)), OriginalColony.Position, new Random(Guid.NewGuid().GetHashCode())));
+
             }
         }
         private void StartOrStopSimButton(object sender, RoutedEventArgs e)
@@ -167,19 +119,12 @@ namespace WpfAntSimulator
         private void AntAmountChange(object sender, TextChangedEventArgs e)
         {
             int i;
-            if(Int32.TryParse(AntAmount.Text, out i))
+            if (Int32.TryParse(AntAmount.Text, out i))
             {
                 numOfAnts = i;
             }
         }
-        private void ResetSimButton(object sender, RoutedEventArgs e)
-        {
-            simObjects.Clear();
-            simStaticObjects.Clear();
-            simInit = false;
-            RenderStatics();
-            RenderAll();
-        }
+
 
 
         // This function is the engine.
@@ -189,7 +134,7 @@ namespace WpfAntSimulator
             {
                 InitSim();
                 simInit = true;
-                
+
             }
 
             UpdateAll();
@@ -206,123 +151,8 @@ namespace WpfAntSimulator
 
 
 
-
-        // Displays image from bitmap.
-        private void DisplayImage(Bitmap b)
-        {
-            myImage.Source = Bitmap2BitmapImage(b); ;
-        }
-        // Converts Bitmaps to BitmapImages.
-        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
-        {
-            using (var memory = new MemoryStream())
-            {
-                bitmap.Save(memory, ImageFormat.Png);
-                memory.Position = 0;
-
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
-
-                return bitmapImage;
-            }
-        }
-        private static bool IsTextAllowed(string text)
-        {
-            return !_regex.IsMatch(text);
-        }
-
-        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = !IsTextAllowed(e.Text);
-        }
-
-        private void SelectObstacle_Click(object sender, RoutedEventArgs e)
-        {
-            selectedObject = new Obstacle();
-        }
-        private void SelectNest_Click(object sender, RoutedEventArgs e)
-        {
-            selectedObject = new Colony();
-        }
-
-        private void myImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (selectedObject == null) return;
-
-            bool changeWasMade = false;
-            var prevSelectedObject = selectedObject;
-            Point prevPos = selectedObject.Position;
-
-            selectedObject.Position = new Point((int)e.GetPosition(myImage).X, (int)e.GetPosition(myImage).Y);
-            switch (selectedObject)
-            {
-                case Obstacle obstacle:
-                    int prevW = (selectedObject as Obstacle).Width;
-                    int prevH = (selectedObject as Obstacle).Height;
-
-                    int newW = Int32.Parse(ObstacleWidth.Text);
-                    int newH = Int32.Parse(ObstacleHeight.Text);
-
-                    (selectedObject as Obstacle).Width = newW;
-                    (selectedObject as Obstacle).Height = newH;
-
-                    if(prevW != newW || prevH != newH || AreDifferent(prevPos, selectedObject.Position))
-                    {
-                        changeWasMade = true;
-                    }
-
-                    break;
-                case Colony colony:
-                    int prevR = (selectedObject as Colony).Radius;
-                    int newR = Int32.Parse(NestRadius.Text);
-                    (selectedObject as Colony).Radius = newR;
-
-                    if (prevR != newR || AreDifferent(prevPos, selectedObject.Position))
-                    {
-                        changeWasMade = true;
-                    }
-
-                    break;
-                default:
-                    return;
-            }
-            if (!simStaticObjects.Contains(selectedObject) || changeWasMade)
-            {
-                if (changeWasMade)
-                {
-                    simStaticObjects.Remove(prevSelectedObject);
-                    changeWasMade = false;
-                }
-                simStaticObjects.Add(selectedObject);
-                RenderStatics();                
-                //simObjects.Add(selectedObject);
-            }
-            //simObjects.Add(selectedObject);
-            RenderAll();
-        }
-
-        private bool AreDifferent(Point a, Point b)
-        {
-            if (a.X != b.X) return true;
-            if (a.Y != b.Y) return true;
-            return false;
-        }
-
-        private Bitmap MergedBitmaps(Bitmap bmp1, Bitmap bmp2)
-        {
-            Bitmap result = new Bitmap(Math.Max(bmp1.Width, bmp2.Width),
-                                       Math.Max(bmp1.Height, bmp2.Height));
-            using (Graphics g = Graphics.FromImage(result))
-            {
-                g.DrawImage(bmp2, Point.Empty);
-                g.DrawImage(bmp1, Point.Empty);
-            }
-            return result;
-        }
-
+        /// This file was getting quite lengthy
+        /// I've moved methods connected to rendering the bitmap to the seperate file
+        /// Same with helper functions and event handlers
     }
 }
