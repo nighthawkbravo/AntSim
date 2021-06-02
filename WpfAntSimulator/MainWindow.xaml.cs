b4ab5673ee0f368bd2c49b4fc49fcbea01ba9edb
VIZaneIV <72400206+VIZaneIV@users.noreply.github.com>
2021-06-02 15:59:20 +0200

    Revert "Some changes"
    This reverts commit b83915ececfc11efaea031ab7a03f778f72a39c7.

--- WpfAntSimulator/MainWindow.xaml.cs
+++ WpfAntSimulator/MainWindow.xaml.cs
@@ -112,8 +112,6 @@ namespace WpfAntSimulator
                     continue;
                 }
                 simObj.Update(mergedBMs);
-                if ((simObj as Ant).CheckDirection((simObj as Ant).Position, bm) != null) ;
-
             }
             if (toBeRemoved.Count > 0)
             {
@@ -149,7 +147,7 @@ namespace WpfAntSimulator
         }
         private void InitSim()
         {
-            simStaticObjects.Add(new Colony(center));
+            simObjects.Add(new Colony(center));
             for (int i=0; i<numOfAnts; ++i)
             {
                 // Random rand = new Random(Guid.NewGuid().GetHashCode()); // Very useful for generating random objects with random seeds!
